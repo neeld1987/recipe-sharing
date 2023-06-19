6b13c71582fda60729a22fbd34560d264ed140d1
neelepl87 <niladridas07@gmail.com>
2023-06-19 09:57:39 +0200

    removed unused code

--- src/main/java/com/example/recipesharing/service/RecipeServiceImpl.java
+++ src/main/java/com/example/recipesharing/service/RecipeServiceImpl.java
@@ -35,32 +35,6 @@ public class RecipeServiceImpl implements RecipeService{
             recipe.addIngredient(ingredients);
             ingredients.setRecipe(recipe);
         }
-        /*
-        Ingredient ingredients1 = new Ingredient();
-        ingredients1.setType("egg");
-        ingredients1.setUnit(IngredientUnit.pc);
-        ingredients1.setValue(8);
-
-        Ingredient ingredients2 = new Ingredient();
-        ingredients2.setType("spaghetti");
-        ingredients2.setUnit(IngredientUnit.g);
-        ingredients2.setValue(500);
-
-
-
-        Recipe recipe = new Recipe();
-        recipe.setDescription("Just make Spaghetti Carbonara");
-        recipe.setInstructions("DIY");
-        recipe.setServings(4);
-        recipe.setUser(existingUser);
-        recipe.setTitle("Spaghetti Carbonara");
-        recipe.addIngredient(ingredients1);
-        recipe.addIngredient(ingredients2);
-
-        ingredients1.setRecipe(recipe);
-        ingredients2.setRecipe(recipe);
-
-         */
 
         Recipe savedRecipe = recipeRepository.save(recipe);
         return savedRecipe;
